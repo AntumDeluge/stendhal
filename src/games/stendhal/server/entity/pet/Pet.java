d4a1de92581baf7119a1d0ffca3bc99ea88ae662
Jordan Irwin <antumdeluge@gmail.com>
2024-06-22 11:40:00 -0700

    New pet code (WIP)

---
+++ src/games/stendhal/server/entity/pet/Pet.java
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *                    Copyright © 2024 - Faiumoni e. V.                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.pet;
+
+import games.stendhal.server.entity.DressedEntity;
+import games.stendhal.server.entity.item.Corpse;
+
+
+public class Pet extends DressedEntity {
+
+	public static void generateRPClass() {
+		// TODO:
+	}
+
+	@Override
+	protected void dropItemsOn(Corpse corpse) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void logic() {
+		// TODO Auto-generated method stub
+	}
+}
